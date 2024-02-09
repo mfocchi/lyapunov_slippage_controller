f5a8166bdd39bf54ae9db56c2e4fbe7886441e8e
Michele Focchi <michele.focchi@unitn.it>
2024-02-09 10:21:18 +0100

    implemented the spiral test for slippage identification

--- launch/differential_ol_lab.launch.py
+++ launch/differential_ol_lab.launch.py
@@ -7,18 +7,45 @@ from datetime import datetime
 
 def generate_launch_description():
     ld = LaunchDescription()
-    dt = 0.05 # [s]
-    long_v = 0.05 # [m/s]
-    # turning_radius = -0.3 # [m]
-    # ang_w = long_v / turning_radius # [rad/s]
-    ang_w = 0.0
-    t_end = 8 # [s]
+    # dt = 0.005 # [s]
+    # long_v = 0.05 # [m/s]
+    # # turning_radius = -0.3 # [m]
+    # # ang_w = long_v / turning_radius # [rad/s]
+    # ang_w = 0.0
+    # t_end = 8 # [s]
+    # n = (np.ceil(t_end / dt)).astype(int)    
+    # v_vec     = np.linspace(long_v,   long_v, n).tolist()
+    # omega_vec = np.linspace(ang_w,   ang_w, n).tolist()
+    # v_vec.append(0.0)
+    # omega_vec.append(0.0)
+
+    R_initial = 0.1
+    R_final = 0.55
+    dt = 0.005  # [s] 200Hz
+    long_v = 0.1  # [m/s]
+    change_percentage = 0.5
+    t_end = 30  # [s]
     n = (np.ceil(t_end / dt)).astype(int)
-    
-    v_vec     = np.linspace(long_v,   long_v, n).tolist()
-    omega_vec = np.linspace(ang_w,   ang_w, n).tolist()
+    np.set_printoptions(threshold=np.inf, precision = 5, linewidth = 10000, suppress = True)
+    turning_radius = np.arange(R_initial, R_final, 0.05)
+    ang_w = np.round(long_v / turning_radius,3)  # [rad/s]
+    omega_vec = []
+    theta = 0
+    i = 0
+    while True:
+        theta += abs(ang_w[i])*dt
+        #print(theta)
+        omega_vec.append(ang_w[i])
+        if (theta > (1+i)*(2*np.pi * change_percentage)):
+            i +=1
+        if i == len(turning_radius):
+            break
+    # print(omega_vec)
+    # print(ang_w)
+    v_vec = np.linspace(long_v, long_v, n).tolist()
     v_vec.append(0.0)
     omega_vec.append(0.0)
+
 
     optitrack_node = model_conv_node = Node(
         package="optitrack_interface",
@@ -52,7 +79,8 @@ def generate_launch_description():
     now = datetime.now()
 
     dt_string = now.strftime("%d-%m-%H-%M")
-    param_string = 'v%1.0f_omega%1.0f_' % (100*v_vec[0], 100*omega_vec[0])
+    #param_string = 'v%1.0f_omega%1.0f_' % (100*v_vec[0], 100*omega_vec[0])
+    param_string = 'long_v%1.0f_Rinit%1.0f_Rend%1.0f_' % (100*long_v, 100*R_initial, 100*R_final)
     bag_string = 'bagfiles/ol_'
     bag_name = bag_string + param_string + dt_string + '.bag'
     record_node = ExecuteProcess(
